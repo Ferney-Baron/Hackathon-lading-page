0f3f8d44e2af70afcacd127dbd952a7d7fea2329
Ferney <ferneydvbaron@gmail.com>
2023-06-07 15:00:06 -0500

    version casi terminada

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // botones();
+    imagenes();
 })
 
 const menu = document.querySelector('.nav-icon');
 const contenido = document.querySelector('.nav-menu');
+const formulario = document.querySelector('.formulario');
 
 
 menu.addEventListener('click', () => {
@@ -15,5 +16,19 @@ menu.addEventListener('click', () => {
 
 window.addEventListener('resize', () => {
     (window.innerWidth > 992) ? contenido.style.display = 'block' : contenido.style.display = 'none';
+
 });
 
+function imagenes() {
+    const patrocinios = document.querySelector('.patrocinadores-img');
+    let contenido = '';
+    
+    for(let i = 1; i < 7; i++) {
+        contenido += `
+            <div class="contenedor-img">
+                <img class="img img${i}" src="build/img/marcas/${i}.png" alt="patrocinador" />
+            </div>
+        `
+        patrocinios.innerHTML = contenido;
+    }
+}
